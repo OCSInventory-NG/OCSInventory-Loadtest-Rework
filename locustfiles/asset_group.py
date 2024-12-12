3e59b4d034f0a8aef2354f6c162ff1cff90fb5b9
Antoine ROBIN <antoine.robin@factorfx.com>
2024-12-12 16:27:01 +0000

    feat(tests): affect users in groups + create packages

--- locustfiles/asset_group.py
+++ locustfiles/asset_group.py
@@ -9,10 +9,39 @@ class AssetGroupAPITest(HttpUser):
     token = None
     search = None
     assets = []
+    os_options = ["windows", "linux", "mac"]
 
     def on_start(self):
         # Retrieve auth token at startup
         self.token = Auth.get_token(self)
+
+    def get_assets(self):
+        """
+        GET /search
+        """
+        self.assets = []
+        if self.token:
+            # Sending the POST request with the authentication token
+            response = self.client.post(
+                "/search/",
+                headers={
+                    "Authorization": f"Token {self.token}",
+                    "Content-Type": "application/json",
+                },
+                data=json.dumps(self.search),
+            )
+
+            if response.status_code == 200:
+                assets = response.json()
+                for asset in assets:
+                    self.assets.append(asset.get("pk"))
+            else:
+                print(
+                    "An error occured when attempt to retrieve assets : ",
+                    response.text,
+                )
+        else:
+            print("Token not available, request not executed")
 
     @task
     def create_group(self):
@@ -22,17 +51,19 @@ class AssetGroupAPITest(HttpUser):
         if self.token:
             # Generate random num between 00001 and 99999
             random_number = f"{random.randint(1, 99999):05}"
+            # Random selection of an operating system for osname
+            osname = random.choice(self.os_options)
 
             self.search = [
                 [
                     {
-                        "link": "",
-                        "field": "name",
-                        "route": "asset/bases",
-                        "value": "PC",
                         "object": "InventoryBase",
-                        "operator": "istartswith",
+                        "route": "asset/bases",
+                        "field": "osname",
                         "fieldtype": "string",
+                        "operator": "iexact",
+                        "value": osname,
+                        "link": "AND",
                     }
                 ]
             ]
@@ -43,8 +74,8 @@ class AssetGroupAPITest(HttpUser):
             data = {
                 "visibility": "public",
                 "allow_group_modification": False,
-                "name": f"Dummy group {random_number}",
-                "description": "Dummy group",
+                "name": f"Dummy {osname} group {random_number}",
+                "description": "Dummy group for API test",
                 "is_dynamic": True,
                 "search": self.search,
                 "user": 1,
@@ -68,32 +99,5 @@ class AssetGroupAPITest(HttpUser):
                     response.text,
                 )
 
-        else:
-            print("Token not available, request not executed")
-
-    def get_assets(self):
-        """
-        GET /search
-        """
-        if self.token:
-            # Sending the POST request with the authentication token
-            response = self.client.post(
-                "/search/",
-                headers={
-                    "Authorization": f"Token {self.token}",
-                    "Content-Type": "application/json",
-                },
-                data=json.dumps(self.search),
-            )
-
-            if response.status_code == 200:
-                assets = response.json()
-                for asset in assets:
-                    self.assets.append(asset.get("pk"))
-            else:
-                print(
-                    "An error occured when attempt to retrieve assets : ",
-                    response.text,
-                )
         else:
             print("Token not available, request not executed")
--- locustfiles/asset_package.py
+++ locustfiles/asset_package.py
@@ -1,16 +1,147 @@
 from locust import HttpUser, task, between
 from common.auth import Auth
 import json
+import random
 
 
 class AssetPackageAPITest(HttpUser):
     wait_time = between(1, 5)
     token = None
+    os_options = ["WIN", "LIN", "MAC"]
+    package_id = None
+    package_name = None
+    groups = []
 
     def on_start(self):
         # Retrieve auth token at startup
         self.token = Auth.get_token(self)
 
+    def search_package_id(self):
+        """
+        GET /deployment/packages
+        """
+        if self.token:
+            # Sending the GET request with the authentication token
+            response = self.client.get(
+                f"/deployment/packages/?name='{self.package_name}'",
+                headers={
+                    "Authorization": f"Token {self.token}",
+                },
+            )
+
+            if response.status_code == 200:
+                self.package_id = response.json()[0].get("id")
+            else:
+                print(
+                    "An error occured when attempt to POST deployment result : ",
+                    response.text,
+                )
+
+        else:
+            print("Token not available, request not executed")
+
+    def search_group_by_os(self):
+        """
+        GET /asset/groups
+        """
+        if self.token:
+            # Sending the GET request with the authentication token
+            response = self.client.get(
+                f"/asset/groups/?name='{self.osname}'",
+                headers={
+                    "Authorization": f"Token {self.token}",
+                },
+            )
+
+            if response.status_code == 200:
+                groups = response.json()
+                for group in groups:
+                    self.groups.append(group.get("id"))
+            else:
+                print(
+                    "An error occured when attempt to POST deployment result : ",
+                    response.text,
+                )
+
+        else:
+            print("Token not available, request not executed")
+
+    def create_group_result(self):
+        """
+        POST /deployment/results
+        """
+        if self.token:
+            # Generate random num between 00001 and 99999
+            random_number = f"{random.randint(1, 99999):05}"
+
+            self.search_package_id()
+            self.search_group_by_os()
+
+            # Data preparation with dynamic incrementation
+            for group in self.groups:
+                data = {
+                    "package": self.package_id,
+                    "group": group,
+                    "name": f"Package {random_number}",
+                    "status": 0,
+                    "comment": "In waiting",
+                }
+                # Sending the POST request with the authentication token
+                response = self.client.post(
+                    "/deployment/results/",
+                    headers={
+                        "Authorization": f"Token {self.token}",
+                        "Content-Type": "application/json",
+                    },
+                    data=json.dumps(data),
+                )
+
+                if response.status_code != 200:
+                    print(
+                        "An error occured when attempt to POST deployment result : ",
+                        response.text,
+                    )
+
+        else:
+            print("Token not available, request not executed")
+
     @task
-    def idle_task(self):
-        pass
+    def create_package_for_group(self):
+        """
+        POST /deployment/packages
+        """
+        if self.token:
+            # Generate random num between 00001 and 99999
+            random_number = f"{random.randint(1, 99999):05}"
+            # Random selection of an operating system for osname
+            self.osname = random.choice(self.os_options)
+
+            # Data preparation with dynamic incrementation
+            data = {
+                "name": f"Dummy Package {random_number}",
+                "description": "Dummy Package for API test",
+                "target_os": self.osname,
+            }
+
+            self.package_name = f"Dummy Package {random_number}"
+
+            # Sending the POST request with the authentication token
+            response = self.client.post(
+                "/deployment/packages/",
+                headers={
+                    "Authorization": f"Token {self.token}",
+                    "Content-Type": "application/json",
+                },
+                data=json.dumps(data),
+            )
+
+            if response.status_code == 200:
+                self.create_group_result()
+            else:
+                print(
+                    "An error occured when attempt to POST deployment package : ",
+                    response.text,
+                )
+
+        else:
+            print("Token not available, request not executed")
