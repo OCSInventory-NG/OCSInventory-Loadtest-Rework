b832ea449a4adb9b612c53f9d36623a19d890963
Antoine ROBIN <antoine.robin@factorfx.com>
2024-12-12 10:50:25 +0000

    feat(group test): finish group test

--- locustfiles/asset_group.py
+++ locustfiles/asset_group.py
@@ -7,6 +7,8 @@ import random
 class AssetGroupAPITest(HttpUser):
     wait_time = between(1, 5)
     token = None
+    search = None
+    assets = []
 
     def on_start(self):
         # Retrieve auth token at startup
@@ -20,6 +22,23 @@ class AssetGroupAPITest(HttpUser):
         if self.token:
             # Generate random num between 00001 and 99999
             random_number = f"{random.randint(1, 99999):05}"
+
+            self.search = [
+                [
+                    {
+                        "link": "",
+                        "field": "name",
+                        "route": "asset/bases",
+                        "value": "PC",
+                        "object": "InventoryBase",
+                        "operator": "istartswith",
+                        "fieldtype": "string",
+                    }
+                ]
+            ]
+
+            self.get_assets()
+
             # Data preparation with dynamic incrementation
             data = {
                 "visibility": "public",
@@ -27,22 +46,10 @@ class AssetGroupAPITest(HttpUser):
                 "name": f"Dummy group {random_number}",
                 "description": "Dummy group",
                 "is_dynamic": True,
-                "search": [
-                    [
-                        {
-                            "link": "",
-                            "field": "name",
-                            "route": "asset/bases",
-                            "value": "PC",
-                            "object": "InventoryBase",
-                            "operator": "istartswith",
-                            "fieldtype": "string",
-                        }
-                    ]
-                ],
+                "search": self.search,
                 "user": 1,
                 "groups": [],
-                "assets": [],
+                "assets": self.assets,
             }
 
             # Sending the POST request with the authentication token
@@ -61,5 +68,32 @@ class AssetGroupAPITest(HttpUser):
                     response.text,
                 )
 
+        else:
+            print("Token not available, request not executed")
+
+    def get_assets(self):
+        """
+        GET /search
+        """
+        if self.token:
+            # Sending the POST request with the authentication token
+            response = self.client.post(
+                "/search/",
+                headers={
+                    "Authorization": f"Token {self.token}",
+                    "Content-Type": "application/json",
+                },
+                data=json.dumps(self.search),
+            )
+
+            if response.status_code == 200:
+                assets = response.json()
+                for asset in assets:
+                    self.assets.append(asset.get("pk"))
+            else:
+                print(
+                    "An error occured when attempt to retrieve assets : ",
+                    response.text,
+                )
         else:
             print("Token not available, request not executed")
